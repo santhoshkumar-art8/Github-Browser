50ebec647f7dd010f83a7d63f4578ae01bef37a8
santhoshkumar-art8 <79752552+santhoshkumar-art8@users.noreply.github.com>
2021-09-11 11:53:09 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+
+
+const sub=document.getElementById('ben');
+
+sub.addEventListener("click",function(e){
+  e.preventDefault();
+uservalue=document.getElementById("user").value;
+
+  
+ 
+
+
+
+
+
+ var request=new XMLHttpRequest();
+
+
+
+
+
+request.open('GET',"https://api.github.com/users/"+uservalue+"/repos",true);
+
+request.onload=function (){
+  var data = JSON.parse(this.response);
+
+  var statusHTML = '';
+  $.each(data, function(i, status) {
+    statusHTML += '<tr>';
+    statusHTML += '<td>' + status.id + '</td>';
+    statusHTML += '<td>' + status.name + '</td>';
+    statusHTML += '<td><a target="_blank" href="status.html_url">'+status.html_url+'</a></td>';
+    statusHTML += '<td>' + status.language + '</td>';
+    statusHTML += '</tr>';
+  });
+  $('tbody').html(statusHTML);
+}
+
+request.send();
+
+
+});
+
+
+
+
